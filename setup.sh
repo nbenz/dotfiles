549537dee2df78233c9656f69d36bd9b765ba380
Nate Benz <nb@chrios.net>
2015-11-10 10:52:09 -0600

    nit

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#Install Vundle if it's not installed
+# Install Vundle if it's not installed
 if [ ! -e ~/.vim/bundle/Vundle.vim ]; then
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim 
 fi
