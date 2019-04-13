9ebcdc2040a1c49637809a59558f6f284158638b
Nate Benz <natembenz@gmail.com>
2019-04-13 16:17:38 -0500

    add gitconfig to setup

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ if [ ! -e ~/.vim/bundle/Vundle.vim ]; then
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim 
 fi
 
+cp .gitconfig ~/
 cp -r .vim ~/
 cp .vimrc ~/
 cp solarize.sh ~/
